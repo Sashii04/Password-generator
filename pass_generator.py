d21c66476bea5641b49689159d12ebc433c2ceb5
Nicolas Neira <134659095+Sashii04@users.noreply.github.com>
2026-03-21 22:15:17 -0300

    Add files via upload

---
+++ pass_generator.py
@@ -0,0 +1,90 @@
+import random
+import os
+def password():
+    continuar = False
+    correr_denuevo = False
+    while correr_denuevo == False:
+        while continuar == False:
+            ask_length = int(input("Insert password length:\n"))
+            ask_uppercase = str(input("Use uppercase?: Y/n\n"))
+            ask_lowercase = input("Use lowercase?: Y/n\n")
+            ask_simbols = input("Use simbols?: Y/n\n")
+            ask_numbers = input("Use numbers?: Y/n\n")
+            letras = "abcdefghijklmnopqrstuvwyxz"
+            simbolos = "!@#$%^&*()_+"
+            numeros = "1234567890"
+            opciones = ""
+            contrasena_final = ""
+             
+            if ask_uppercase.lower() == "y":
+                
+                 ask_uppercase = "Si"
+                 opciones += letras.upper()
+            else:
+                
+                ask_uppercase = "No"
+
+            if ask_lowercase.lower() == "y":
+                
+                ask_lowercase = "Si"
+                opciones += letras
+            
+            else:
+                ask_lowercase = "No"
+
+            if ask_simbols.lower() == "y":
+                ask_simbols = "Si"
+                opciones += simbolos
+            
+            else:
+                ask_simbols = "No"
+            
+            if ask_numbers.lower() == "y":
+                ask_numbers = "Si"
+                opciones += numeros 
+            else:
+                ask_numbers = "No"
+
+            if opciones == "":
+                input("Can't have an empty password! Enter to try again")
+                os.system('cls')
+                continue
+            os.system('cls')
+            continuar = input(f"\nYour password has {ask_length} caracteres.\n Caps?: {ask_uppercase}.\n Lowercase?: {ask_lowercase}.\n Simbols?: {ask_simbols}.\n Numbers? {ask_numbers}.\n\n  Continue?: Y/n ")
+
+            
+            
+            if continuar.lower() == "y":
+                continuar = True
+                os.system('cls')
+
+                for _ in range(ask_length):
+                    caracteres_random = random.choice(opciones)
+                    contrasena_final += caracteres_random
+            
+                print(contrasena_final) 
+
+            else: 
+                continuar = False
+                os.system('cls')
+                continue
+                
+
+            correr_denuevo = input("Generate another password?: Y/n\n")
+
+            if correr_denuevo.lower() == "y":
+                os.system('cls')
+                correr_denuevo = False 
+                continuar = False
+
+            else:
+                correr_denuevo = True
+                os.system('cls')
+                
+            
+
+    
+password()
+
+input("\n\nPresiona Enter para salir")
+
